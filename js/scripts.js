a9cef2b10561d9e59946526023b3b512c631647d
Moringa School <dorcasmosonik@gmail.com>
2020-09-06 00:45:43 +0300

    order module

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,51 @@
 // Material Select Initialization
-$(document).ready(function() {
+$(document).ready(function () {
     $('.mdb-select').materialSelect();
-    });
+    //pizza
+
+    class Pizza {
+        constructor(types, toppings, crust, size,total) {
+            this.types = types;
+            this.toppings = toppings;
+            this.crust = crust;
+            this.size = size;
+            this.total = total;
+
+        }
+    }
+
+    Pizza.prototype.getTypePrice = function () {
+
+        var typeNames = {
+            name: ["BBQ", "Hawaaian", "ChickenTikka"]
+        };
+        var toppingNames = {
+            name: ["mushroom", "sausage","periperi"]
+        };
+        var crustNames = {
+            name: ["thick", "thin"]
+        };
+        var sizeNames = {
+            name: ["large", "medium", "small"]
+        };
+        sizeNames.name.forEach(function (sizeName) {
+            if (sizeName[0]) {
+                typeNames.name.forEach(function (typeName) {
+                    if (typeName[0])
+                        return this.types; 
+                })
+            } else if (sizeName[1]) {
+                typeNames.name.forEach(function (typeName) {
+                    if (typeName[0])
+                        return 1300;
+                })
+            } else {
+                return 1000;
+            }
+
+        });
+
+    }
+    var pizz = new Pizza("BBQ", "mush", "thick", "small");
+    alert(pizz.getTypePrice());
+});
