515f65cdd551dd67c5cda773d13a1798196dd679
Moringa School <dorcasmosonik@gmail.com>
2020-09-06 16:03:48 +0300

    add pizza table

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 // Material Select Initialization
 $(document).ready(function () {
+    $("#table1").hide();
+    $('#del').click(function () {
+        $(this).remove();
+    });
+   
 
     function Pizza(type, size, topping, crust) {
         this.type = type;
@@ -7,7 +12,23 @@ $(document).ready(function () {
         this.topping = topping;
         this.crust = crust;
     }
+    var pizza = new Pizza(typeSelected, sizeSelected, toppingSelected, crustSelected);
+    var typeSelected = $("#type").val();
+    var sizeSelected = $("#size").val();
+    var toppingSelected = $("#topping").val();
+    var crustSelected = $("#crust").val();
+    Pizza.prototype.getPizzaPrice = function () {
+        return this.getTypePrice() + this.getToppingPrice() + this.getCrustType();
+    }
+    Pizza.prototype.getSubTotal = function () {
+        var totalCost =0;
+        var pizzaOrdered = [];
+        pizzaOrdered.push(pizza);
 
+        for (var i = 0; i < total.length; i++) {
+        return  totalCost += pizzaOrdered[i].getPizzaPrice();
+          }
+    }
     Pizza.prototype.getTypePrice = function () {
         if (this.type === "BBQ") {
             if (this.size === "large") {
@@ -37,7 +58,7 @@ $(document).ready(function () {
                 return 1300
 
             }
-        }else{
+        } else {
             if (this.size === "large") {
                 return 1000
             } else if (this.size === "medium")
@@ -48,8 +69,41 @@ $(document).ready(function () {
             }
         }
     }
+    Pizza.prototype.getToppingPrice = function () {
+        if (this.size === "large") {
+            if (this.topping === "pepperoni") {
+                return 150;
+            } else if (this.topping === "mushroom") {
+                return 120;
+            } else {
+                return 180;
+            }
+        } else if (this.size === "medium") {
+            if (this.topping === "pepperoni") {
+                return 100;
+            } else if (this.topping === "mushroom") {
+                return 80;
+            } else {
+                return 140;
+            }
+        } else {
+            if (this.topping === "pepperoni") {
+                return 80;
+            } else if (this.topping === "mushroom") {
+                return 50;
+            } else {
+                return 100;
+            }
+        }
+    }
+    Pizza.prototype.getCrustType = function () {
+        if (this.crust === "thick") {
+            return 100;
+        } else {
+            return 0;
+        }
 
-    // console.log(pizza.typeSelected);
+    }
 
     $("#pizza-form").submit(function (e) {
         e.preventDefault();
@@ -57,8 +111,33 @@ $(document).ready(function () {
         var sizeSelected = $("#size").val();
         var toppingSelected = $("#topping").val();
         var crustSelected = $("#crust").val();
+        var total = "";
         var pizza = new Pizza(typeSelected, sizeSelected, toppingSelected, crustSelected);
-        console.log(pizza.getTypePrice());
+        $("#table").append(
+            "<tr>" +
+            '<td scope="row">' + "#" +
+            "</td>" +
+            '<td scope="row">' +
+            pizza.type +
+            "</td>" +
+            '<td scope="row">' +
+            pizza.size +
+            "</td>" +
+            '<td scope="row">' +
+            pizza.topping +
+            "</td>" +
+            '<td scope="row">' +
+            pizza.crust +
+            "</td>" +
+            "<td>" +
+            pizza.getPizzaPrice() +
+            "</td>" +
+            "</tr>"
+        );
+        $("#table1").show();
+    });
+    $("#checkout").click(function () {
+        alert(pizza.s)
     });
 
 });
